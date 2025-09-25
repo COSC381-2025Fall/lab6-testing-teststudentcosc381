86beb3867df8a1ae47de43b8b5ea3061e2d64cc0
Test Student <144061513+teststudentcosc381@users.noreply.github.com>
2025-09-25 13:59:18 +0000

    Part 2: Refactor for Testability

--- presidio-anonymizer/presidio_anonymizer/sample.py
+++ presidio-anonymizer/presidio_anonymizer/sample.py
@@ -1,7 +1,13 @@
 from presidio_anonymizer import AnonymizerEngine
 from presidio_anonymizer.entities import RecognizerResult, OperatorConfig
 
-def sample_run_anonymizer():
+# Part 2: Refactor for Testability
+def sample_run_anonymizer(text:str, start: int, end: int): 
+    # Grading 1: have three parameters, and the parameters should include:
+    # 1) text with a str type,
+    # 2) start with an int type,
+    # 3) end with an int type
+
     # Initialize the engine
     engine = AnonymizerEngine()
 
@@ -9,12 +15,13 @@ def sample_run_anonymizer():
     # analyzer results (potentially coming from presidio-analyzer) and
     # Operators to get the anonymization output:
     result = engine.anonymize(
-        text=input("text: "),
-        analyzer_results=[RecognizerResult(entity_type="PERSON", start=int(input("start: ")), end=int(input("end: ")), score=0.8)],
+        text=text, # Grading 2: change the input method to the parameter
+        analyzer_results=[RecognizerResult(entity_type="PERSON", start=start, end=end, score=0.8)], # Grading 3: change the input methods to the parameters
         operators={"PERSON": OperatorConfig("replace", {"new_value": "BIP"})}
     )
 
     print(result)
+    return result # Grading 3: have a return
 
     # input should be:
     # text: My name is Bond.
@@ -29,4 +36,6 @@ def sample_run_anonymizer():
     # ]
 
 if __name__ == "__main__": 
-    sample_run_anonymizer();
+    result = sample_run_anonymizer("My name is Bond.", 11, 15); # Grading 4: should modify the main method to get the result
+
+# Grading 5: The whole program should be syntactically correct
